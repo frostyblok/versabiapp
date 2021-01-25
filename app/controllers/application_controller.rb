4aea3430a42d4bf3629be87f05d16c028add2902
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-01-25 21:59:02 +0100

    Ensure users can login and signup - setup bootstrap with webpacker - setup simple_form for rails form - setup Devise for users authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  helper_method :devise_mapping
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'page#home'
+  devise_scope :user do
+    get 'user_signup', to: 'devise/registrations#new'
+    get 'user_signin', to: 'devise/sessions#new'
+    get 'user_logout', to: 'devise/sessions#destroy'
+  end
+
+  authenticated :user do
+    root to: 'page#home', as: 'authenticated_route'
+  end
+  root to: 'page#see'
 end
