d06667818e427204eed7205404e681ccc178e7f4
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-01-27 16:39:41 +0100

    Change firstname and lastname to company name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name])
   end
 end
